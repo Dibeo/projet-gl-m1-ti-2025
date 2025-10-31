5c56138db034776ac202e875293e41d144533d4d
Fourre Simon <simon.fourre@etud.univ-pau.fr>
2025-10-31 16:53:43 +0100

    add : Logger basic configuration for server

---
+++ Serveur/src/main/java/org/app/Main.java
@@ -0,0 +1,28 @@
+package org.app;
+
+import org.app.utils.LoggerConfig;
+
+import java.util.logging.Logger;
+
+public class Main {
+    private static final Logger LOGGER = Logger.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        boolean consoleLogging = false;
+        for (String arg : args) {
+            if (arg.equalsIgnoreCase("--logs") || arg.equalsIgnoreCase("-l")) {
+                consoleLogging = true;
+                break;
+            }
+        }
+
+        // Initialize global logger
+        LoggerConfig.setup(consoleLogging);
+
+        // Example logs
+        LOGGER.info("Application started");
+        //LOGGER.warning("This is a warning message");
+        //LOGGER.severe("This is a critical error!");
+
+    }
+}
